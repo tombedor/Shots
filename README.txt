2bad45e0117d04713ee2b9080b3eb56de65f77ad
Tom Bedor <tom@healthloop.com>
2016-01-09 16:55:44 -0800

    Add a few lines of probably shitty code while high

--- README.txt
+++ README.txt
@@ -1,1 +1,8 @@
 This is where I can keep all sorts of interesting information about how I can know what the fuck I should do if I want to actually work on this.
+
+Yakit
+TTS = yakit api, https://www.yakitome.com/upload/from_text
+Docs are more or less nonexistent, but you can log in with
+coolthingofthemonth email and get your api token. Secrets
+file is in google drive.
+
--- app/models/yakit.rb
+++ app/models/yakit.rb
@@ -1,5 +1,41 @@
 class Yakit
+  # /tts is the function name, there might be others besides tts
+  YAKIT_URL= 'https://www.yakitome.com/api/rest/tts'
 
+  require 'net/https'
+  require 'open-uri'
+
+  def api_key
+    return @api_key if @api_key
+    @api_key = HashWithIndifferentAccess.new(YAML.load(File.read(File.expand_path("#{Rails.root}/config/secrets.yml", __FILE__))))['yakit_key']
+  end
+
+  def get_recording(text)
+    data = {
+      'api_key' => api_key,
+      'voice' => 'Mike',
+      'speed' => 5,
+      'text' => text}
+
+
+    url = URI.parse(YAKIT_URL)
+    req = Net::HTTP::Post.new(url.path)
+    req.form_data = data
+    req.basic_auth url.user, url.password if url.user
+    con = Net::HTTP.new(url.host, url.port)
+    con.use_ssl = true
+    con.start {|http| http.request(req)}
+  end
+
+
+
+
+
+
+
+
+
+  ##### Copied from python,
 
   import httplib
   import urllib
@@ -31,4 +67,10 @@ class Yakit
   def initialize
     @key = HashWithIndifferentAccess.new(YAML.load(File.read(File.expand_path("#{Rails.root}/config/secrets.yml", __FILE__))))['yakit_key']
   end
+
+  def sample
+    rest('POST', 'tts', vars)
+
+  end
+
 end
