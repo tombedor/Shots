7562c50a1b9fecb69c889659ec9a3bec6e6332df
Tom Bedor <tombedor@Toms-MacBook-Air.local>
2019-12-29 22:37:45 -0800

    add happy birthday, allow spaces

--- rap.sh
+++ rap.sh
@@ -1,9 +1,13 @@
 # !/bin/bash
-set -euo pipefail
+set -exuo pipefail
 
 # requires say and sox
 
 BPM=128
+SONG=shots_instruments_128_bpm_sampled.aiff
+
+#BPM=60
+#SONG=smooth_jazz.aiff
 
 mkdir -p tmp
 
@@ -19,24 +23,22 @@ i=0
 files=""
 while IFS=, read -r word beats
 do
-
 	if [ "$word" = "REST" ]; then
-		echo "YAY"
 		rest_length=`echo "$beats*${BEAT_LENGTH_SECONDS}" | bc -l`
-		echo $rest_length	
 		sox -n -r 22050 tmp/${i}.aiff trim 0.0 $rest_length
 	else
-		write_word $word
-		clip_length=`sox tmp/$word.aiff -n stat 2>&1 | grep Length | awk '{print $3}'`
-		scale_factor=`echo "${clip_length}/(${BEAT_LENGTH_SECONDS}*$beats)" | bc -l`
-		echo $scale_factor
-		sox tmp/$word.aiff tmp/$i.aiff tempo $scale_factor
+		echo $word
+		say "$word" -o tmp/${i}_tmp.aiff
+		sox tmp/${i}_tmp.aiff tmp/${i}_trimmed.aiff silence 1 0.1 1%
+		clip_length=`sox tmp/${i}_trimmed.aiff -n stat 2>&1 | grep Length | awk '{print $3}'`
+		scale_factor=`echo "${clip_length}/(${BEAT_LENGTH_SECONDS}*${beats})" | bc -l`
+		sox tmp/${i}_trimmed.aiff tmp/${i}.aiff tempo $scale_factor
 	fi
 	files="${files} tmp/${i}.aiff"
 	i=$((i + 1))
-	
 done < "${1:-/dev/stdin}"
-sox $files tmp/speech.aiff repeat 10
 
-sox -m tmp/speech.aiff shots_instruments_128_bpm_sampled.aiff tmp/mixed.aiff
+
+sox $files tmp/speech.aiff repeat 10
+sox -m tmp/speech.aiff $SONG tmp/mixed.aiff
 play tmp/mixed.aiff
