1335f1d0d686e57f9deb9d856296df5a8e7a5a2a
Tom Bedor <tombedor@gmail.com>
2020-01-02 12:05:36 -0800

    fixes for file rearrangement

--- rap.sh
+++ rap.sh
@@ -3,18 +3,23 @@ set -exuo pipefail
 
 # requires say and sox
 
+
+SOUND_CLIPS_DIR="./sounds"
+TMP_DIR="./tmp"
+
 #BPM=128
-#SONG=shots_instruments_128_bpm_sampled.aiff
+#SONG=${SOUND_CLIPS_DIR}/shots_instruments_128_bpm_sampled.aiff
 
 BPM=60
-SONG=smooth_jazz.aiff
+SONG=${SOUND_CLIPS_DIR}/smooth_jazz.aiff
+
 
 mkdir -p tmp
 
 BEAT_LENGTH_SECONDS=`echo "60.0/${BPM}.0" | bc -l`
 
 write_word() {
-	say $1 -o tmp/$1_tmp.aiff
+	say $1 -o ${TMP_DIR}/$1_tmp.aiff
 	sox tmp/$1_tmp.aiff tmp/$1.aiff silence 1 0.1 1%
 }
 
@@ -25,22 +30,22 @@ while IFS=, read -r word beats
 do
 	if [ "$word" = "REST" ]; then
 		rest_length=`echo "$beats*${BEAT_LENGTH_SECONDS}" | bc -l`
-		sox -n -r 22050 tmp/${i}.aiff trim 0.0 $rest_length
+		sox -n -r 22050 ${TMP_DIR}/${i}.aiff trim 0.0 $rest_length
 	else
 		echo $word
-		say "$word" -o tmp/${i}_tmp.aiff
-		sox tmp/${i}_tmp.aiff tmp/${i}_trimmed.aiff silence 1 0.1 1%
-		clip_length=`sox tmp/${i}_trimmed.aiff -n stat 2>&1 | grep Length | awk '{print $3}'`
+		say "$word" -o ${TMP_DIR}/${i}_tmp.aiff
+		sox ${TMP_DIR}/${i}_tmp.aiff ${TMP_DIR}/${i}_trimmed.aiff silence 1 0.1 1%
+		clip_length=`sox ${TMP_DIR}/${i}_trimmed.aiff -n stat 2>&1 | grep Length | awk '{print $3}'`
 		scale_factor=`echo "${clip_length}/(${BEAT_LENGTH_SECONDS}*${beats})" | bc -l`
-		sox tmp/${i}_trimmed.aiff tmp/${i}.aiff tempo $scale_factor
+		sox ${TMP_DIR}/${i}_trimmed.aiff ${TMP_DIR}/${i}.aiff tempo $scale_factor
 	fi
-	files="${files} tmp/${i}.aiff"
+	files="${files} ${TMP_DIR}/${i}.aiff"
 	i=$((i + 1))
 done < "${1:-/dev/stdin}"
 
-sox airhorn.aiff airhorn_repeat.aiff repeat 10
-sox yeah_what_ok.aiff yeah_what_ok_repeat.aiff repeat 20
-sox $files tmp/speech.aiff repeat 10
-#sox -m tmp/speech.aiff $SONG yeah_what_ok_repeat.aiff tmp/mixed.aiff
-sox -m tmp/speech.aiff $SONG tmp/mixed.aiff
-play tmp/mixed.aiff
+sox ${SOUND_CLIPS_DIR}/airhorn.aiff ${TMP_DIR}/airhorn_repeat.aiff repeat 10
+sox ${SOUND_CLIPS_DIR}/yeah_what_ok.aiff ${TMP_DIR}/yeah_what_ok_repeat.aiff repeat 20
+sox $files ${TMP_DIR}/speech.aiff repeat 10
+#sox -m ${TMP_DIR}/speech.aiff $SONG ${TMP_DIR}/yeah_what_ok_repeat.aiff ${TMP_DIR}/mixed.aiff
+sox -m ${TMP_DIR}/speech.aiff $SONG ${TMP_DIR}/mixed.aiff
+play ${TMP_DIR}/mixed.aiff
